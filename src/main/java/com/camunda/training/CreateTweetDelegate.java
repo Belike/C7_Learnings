28258fd70624357f526b92d8bfdcfdd2c41b0a9d
norman.luering@tu-dortmund.de <norman.luering@camunda.com>
2021-11-26 14:55:31 +0100

    Exception Handling Demo

--- src/main/java/com/camunda/training/CreateTweetDelegate.java
+++ src/main/java/com/camunda/training/CreateTweetDelegate.java
@@ -1,10 +1,12 @@
 package com.camunda.training;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 
@@ -23,11 +25,19 @@ public class CreateTweetDelegate implements JavaDelegate {
     }
 
     @Override
-    public void execute(DelegateExecution delegateExecution) throws Exception {
-        //Good World would call Business Logic
-        String content = (String) delegateExecution.getVariable("content");
-        log.info("Publishing tweet: " + content);
-        Long l1 = twitterService.tweet(content);
-        delegateExecution.setVariable("twitterStatus", l1);
+    public void execute(DelegateExecution delegateExecution) throws TwitterException {
+        try {
+            //Good World would call Business Logic
+            String content = (String) delegateExecution.getVariable("content");
+            log.info("Publishing tweet: " + content);
+            Long l1 = twitterService.tweet(content);
+            delegateExecution.setVariable("twitterStatus", l1);
+        }catch (TwitterException ex){
+            if(ex.getErrorCode() == 187){
+                throw new TwitterException("Error");
+            }else{
+                throw ex;
+            }
+        }
     }
 }
---
+++ src/main/java/com/camunda/training/Exceptions/TwitterDuplicateException.java
@@ -0,0 +1,7 @@
+package com.camunda.training.Exceptions;
+
+public class TwitterDuplicateException extends Exception {
+    public TwitterDuplicateException(){
+        String message = "This is a TwitterDuplicate";
+    }
+}
